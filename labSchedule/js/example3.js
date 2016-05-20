a9b2e73954d283337632e76bb24bb04295662d53
System Administrator <root@STANDARD.local>
2016-05-20 16:23:44 -0400

    adding lab schedule

---
+++ labSchedule/js/example3.js
@@ -0,0 +1,120 @@
+var tasks = [
+{"startDate":new Date("Fri May 20 08:30:00 EST 2016"),"endDate":new Date("Fri May 20 19:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Fri May 20 07:30:00 EST 2016"),"endDate":new Date("Fri May 20 20:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 20 08:00:00 EST 2016"),"endDate":new Date("Fri May 20 19:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 20 09:00:00 EST 2016"),"endDate":new Date("Sat May 21 01:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 20 08:45:00 EST 2016"),"endDate":new Date("Fri May 20 22:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 21 08:30:00 EST 2016"),"endDate":new Date("Sat May 21 18:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Sat May 21 07:30:00 EST 2016"),"endDate":new Date("Sat May 21 19:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 21 08:00:00 EST 2016"),"endDate":new Date("Sat May 21 18:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 21 09:00:00 EST 2016"),"endDate":new Date("Sat May 21 21:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 21 08:45:00 EST 2016"),"endDate":new Date("Sat May 21 20:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 22 08:30:00 EST 2016"),"endDate":new Date("Fri May 22 19:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Fri May 22 07:30:00 EST 2016"),"endDate":new Date("Fri May 22 20:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 22 08:00:00 EST 2016"),"endDate":new Date("Fri May 22 19:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 22 09:00:00 EST 2016"),"endDate":new Date("Fri May 22 23:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 22 08:45:00 EST 2016"),"endDate":new Date("Fri May 22 22:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 23 08:30:00 EST 2016"),"endDate":new Date("Sat May 23 18:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Sat May 23 07:30:00 EST 2016"),"endDate":new Date("Sat May 23 19:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 23 08:00:00 EST 2016"),"endDate":new Date("Sat May 23 18:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 23 09:00:00 EST 2016"),"endDate":new Date("Sat May 23 21:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 23 08:45:00 EST 2016"),"endDate":new Date("Sat May 23 20:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 24 08:30:00 EST 2016"),"endDate":new Date("Fri May 24 19:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Fri May 24 07:30:00 EST 2016"),"endDate":new Date("Fri May 24 20:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 24 08:00:00 EST 2016"),"endDate":new Date("Fri May 24 19:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 24 09:00:00 EST 2016"),"endDate":new Date("Sat May 25 01:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 24 08:45:00 EST 2016"),"endDate":new Date("Fri May 24 22:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 25 08:30:00 EST 2016"),"endDate":new Date("Sat May 25 18:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Sat May 25 07:30:00 EST 2016"),"endDate":new Date("Sat May 25 19:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 25 08:00:00 EST 2016"),"endDate":new Date("Sat May 25 18:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 25 09:00:00 EST 2016"),"endDate":new Date("Sat May 25 21:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 25 08:45:00 EST 2016"),"endDate":new Date("Sat May 25 20:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 26 08:30:00 EST 2016"),"endDate":new Date("Fri May 26 19:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Fri May 26 07:30:00 EST 2016"),"endDate":new Date("Fri May 26 20:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 26 08:00:00 EST 2016"),"endDate":new Date("Fri May 26 19:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Fri May 26 09:00:00 EST 2016"),"endDate":new Date("Fri May 26 23:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Fri May 26 08:45:00 EST 2016"),"endDate":new Date("Fri May 26 22:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 27 08:30:00 EST 2016"),"endDate":new Date("Sat May 27 18:00:00 EST 2016"),"taskName":"Arnhold Hall Labs","status":"RUNNING"},
+{"startDate":new Date("Sat May 27 07:30:00 EST 2016"),"endDate":new Date("Sat May 27 19:00:00 EST 2016"),"taskName":"Vera List Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 27 08:00:00 EST 2016"),"endDate":new Date("Sat May 27 18:30:00 EST 2016"),"taskName":"List Innovation Center","status":"RUNNING"},
+{"startDate":new Date("Sat May 27 09:00:00 EST 2016"),"endDate":new Date("Sat May 27 21:30:00 EST 2016"),"taskName":"Parsons East Lab","status":"RUNNING"},
+{"startDate":new Date("Sat May 27 08:45:00 EST 2016"),"endDate":new Date("Sat May 27 20:00:00 EST 2016"),"taskName":"The University Center","status":"RUNNING"},
+
+];
+
+var taskStatus = {
+    "SUCCEEDED" : "bar",
+    "FAILED" : "bar-failed",
+    "RUNNING" : "bar-running",
+    "KILLED" : "bar-killed"
+};
+
+var taskNames = [ "Arnhold Hall Labs", "Vera List Lab", "List Innovation Center", "Parsons East Lab", "The University Center" ];
+
+tasks.sort(function(a, b) {
+    return a.endDate - b.endDate;
+});
+var maxDate = tasks[tasks.length - 1].endDate;
+tasks.sort(function(a, b) {
+    return a.startDate - b.startDate;
+});
+// var minDate = tasks[0].startDate;
+
+var minDate = Date.now();
+
+var format = "%H:%M";
+var timeDomainString = "1day";
+
+var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format).height(650).width(800);
+
+
+gantt.timeDomainMode("fixed");
+changeTimeDomain(timeDomainString);
+
+gantt(tasks);
+
+function changeTimeDomain(timeDomainString) {
+    this.timeDomainString = timeDomainString;
+    switch (timeDomainString) {
+    case "1hr":
+  format = "%H:%M";
+  // gantt.timeDomain([ d3.time.hour.offset(getEndDate(), -1), getEndDate() ]);
+  gantt.timeDomain([ Date.now(), d3.time.hour.offset(Date.now(), 1) ]);
+  break;
+    case "3hr":
+  format = "%H:%M";
+  // gantt.timeDomain([ d3.time.hour.offset(getEndDate(), -3), getEndDate() ]);
+  gantt.timeDomain([ Date.now(), d3.time.hour.offset(Date.now(), 3) ]);
+  break;
+
+    case "6hr":
+  format = "%H:%M";
+  // gantt.timeDomain([ d3.time.hour.offset(getEndDate(), -6), getEndDate() ]);
+  gantt.timeDomain([ Date.now(), d3.time.hour.offset(Date.now(), 6) ]);
+  break;
+
+    case "1day":
+  format = "%H:%M";
+  // gantt.timeDomain([ d3.time.day.offset(getEndDate(), -1), getEndDate() ]);
+  gantt.timeDomain([ Date.now(), d3.time.day.offset(Date.now(), 1) ]);
+  break;
+
+    case "1week":
+  format = "%m/%d";
+  // gantt.timeDomain([ d3.time.day.offset(getEndDate(), -7), getEndDate() ]);
+  gantt.timeDomain([ Date.now(), d3.time.day.offset(Date.now(), 7) ]);
+  break;
+    default:
+  format = "%m/%d"
+
+    case "1month":
+  format = "%m/%d";
+  // gantt.timeDomain([ d3.time.day.offset(getEndDate(), -30), getEndDate() ]);
+  gantt.timeDomain([ Date.now(), d3.time.day.offset(Date.now(), 30) ]);
+  break;
+
+    }
+    gantt.tickFormat(format);
+    gantt.redraw(tasks);
+
+}
