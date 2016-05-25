9e00bd057b44d67966a55ea9c8810bb311f3d190
System Administrator <root@STANDARD.local>
2016-05-25 16:43:15 -0400

    adding isotope filter tool

---
+++ table-isotope/js/script.js
@@ -0,0 +1,28 @@
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan1: function() {
+    var number = $(this).find('.seats').text();
+    return parseInt( number, 10 ) > 1;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+
+// bind filter on radio button click
+$('.filters').on( 'click', 'input', function() {
+  // get filter value from input value
+  var filterValue = this.value;
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
